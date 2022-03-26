0f2d788a3204a684441834a61f6276098ed37bd7
rantmachado <talktorandolph@live.com>
2022-03-26 11:58:52 -0300

    [feat/development] Primeiro commit, iniciando projeto.

---
+++ src/assets/js/page/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../scss/page/home.scss';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function Home() {
+  return (
+    <>
+      <Header />
+      
+      <Footer />
+    </>
+  )
+}
+
+export default Home;
